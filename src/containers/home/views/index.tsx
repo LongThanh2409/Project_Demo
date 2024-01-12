99249686d3dbab042be5d3420f3aa25f7036bdba
LongThanh <longka14pro@gmail.com>
2024-01-12 16:16:58 +0700

    test

--- src/containers/home/views/index.tsx
+++ src/containers/home/views/index.tsx
@@ -7,9 +7,7 @@ type Props = {
 export const Home: FC<Props> = () => {
   return (
     <div className="home">
-      <div className="home__content">
-        hế lo anh trọng việt112345467dsasfrgdtfhyguj
-      </div>
+      <div className="home__content">hế lo</div>
     </div>
   );
 };
